32f7a86910e0c61a06873a7799087588388e8b1b
fabiobonina <fabiobonina@gmail.com>
2017-09-06 07:11:47 -0300

    teste

---
+++ src/components/Hello.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-layout
+    ref="layout"
+    view="lHh Lpr fff"
+    :left-class="{'bg-grey-2': true}"
+  >
+    <q-toolbar slot="header" class="glossy">
+      <q-btn
+        flat
+        @click="$refs.layout.toggleLeft()"
+      >
+        <q-icon name="menu" />
+      </q-btn>
+
+      <q-toolbar-title>
+        Reatorio de Despesa
+        <div slot="subtitle">Dinheiro Vivo</div>
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <div slot="left">
+      <!--
+        Use <q-side-link> component
+        instead of <q-item> for
+        internal vue-router navigation
+      -->
+
+      <q-list no-border link inset-delimiter>
+        <q-list-header>Essential Links</q-list-header>
+        <q-item @click="launch('http://quasar-framework.org')">
+          <q-item-side icon="school" />
+          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+        </q-item>
+        <q-item @click="launch('http://forum.quasar-framework.org')">
+          <q-item-side icon="record_voice_over" />
+          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+        </q-item>
+        <q-item @click="launch('https://gitter.im/quasarframework/Lobby')">
+          <q-item-side icon="chat" />
+          <q-item-main label="Gitter Channel" sublabel="Quasar Lobby" />
+        </q-item>
+        <q-item @click="launch('https://twitter.com/quasarframework')">
+          <q-item-side icon="rss feed" />
+          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        </q-item>
+      </q-list>
+    </div>
+
+    <!--
+      Replace following <div> with
+      <router-view /> component
+      if using subRoutes
+    -->
+    <div>
+      <div class="container">
+        <p>Tes teonandakl dfsmdamweinkn d dfsalknflkasdnfie enjjshdfsdfask fabio fujnsdnfe  sdaksjdfsdfnla</p>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import { dom, event, openURL, QLayout, QToolbar, QToolbarTitle, QBtn, QIcon, QList, QListHeader, QItem, QItemSide, QItemMain } from 'quasar'
+
+
+
+export default {
+  name: 'index',
+  components: {QLayout, QToolbar, QToolbarTitle, QBtn, QIcon, QList, QListHeader, QItem, QItemSide, QItemMain },
+  data () {
+    return {
+      orienting: window.DeviceOrientationEvent && !this.$q.platform.is.desktop,
+      rotating: window.DeviceMotionEvent && !this.$q.platform.is.desktop
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    launch (url) {
+      openURL(url)
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+  .container {
+    padding:20px
+  }
+
+</style>
